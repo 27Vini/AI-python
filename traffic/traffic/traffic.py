aba13265c14fa38f595fb509d03056b190a96790
Vinicius Ouverney <vouverney27@gmail.com>
2023-12-26 15:20:49 -0300

    traffic

--- traffic/traffic/traffic.py
+++ traffic/traffic/traffic.py
@@ -58,7 +58,22 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    images=[]
+    labels=[]
+    
+    for category in range(NUM_CATEGORIES):
+        category_dir = os.path.join(data_dir,str(0))
+        if os.path.dir(category_dir):
+            for fileI in os.listdir(category_dir):
+                image_caminho = os.path.join(category_dir,fileI)
+                image = cv2.imread(image_caminho)
+                image = cv2.resize(IMG_WIDTH,IMG_HEIGHT)
+                images.append(image)
+                labels.append(category)
+    return images,labels
+
+                
+        
 
 
 def get_model():
@@ -67,7 +82,36 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    # Create a convolutional neural network
+    model = tf.keras.models.Sequential([
+
+    # Convolutional layer. Learn 32 filters using a 3x3 kernel
+    tf.keras.layers.Conv2D(
+        32, (3, 3), activation="relu", input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)
+    ),
+
+    # Max-pooling layer, using 2x2 pool size
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+    # Flatten units
+    tf.keras.layers.Flatten(),
+
+    # Add a hidden layer with dropout
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dropout(0.5),
+
+    # Add an output layer with output units for all 10 digits
+    tf.keras.layers.Dense(10, activation="softmax")
+    ])
+    
+    # Train neural network
+    model.compile(
+    optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+    )
+    
+    return model
 
 
 if __name__ == "__main__":
